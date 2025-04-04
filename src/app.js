70aada9c1d2ce298dac04f7ac7099cb5028a8192
martanvcs <martanvcs@gmail.com>
2025-04-04 14:50:03 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon', 'puedes', 'code'];
+  let extensions = ['.com', '.net', '.us', '.io', '.es'];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let hacked = false;
+
+       
+        for (let ext of extensions) {
+          let extText = ext.replace('.', '');
+          if (n.endsWith(extText)) {
+            let hackedName = n.slice(0, -extText.length);
+            console.log(p + a + hackedName + ext);
+            hacked = true;
+           
+          }
+        }
+
+        if (!hacked) {
+          for (let ext of extensions) {
+            console.log(p + a + n + ext);
+          }
+        }
+      }
+    }
+  }
 };
+
+
+//en total se me han generado 64 
